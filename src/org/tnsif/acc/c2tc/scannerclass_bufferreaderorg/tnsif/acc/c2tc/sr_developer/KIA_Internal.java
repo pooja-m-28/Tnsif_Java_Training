6374cf1a362ae7fe325b8fb967d44f29fcb0d860
Pooja M <Pooja M@POOJA-M>
2026-09-22 20:38:16 +0530

    First Commit

---
+++ src/org/tnsif/acc/c2tc/scannerclass_bufferreaderorg/tnsif/acc/c2tc/sr_developer/KIA_Internal.java
@@ -0,0 +1,14 @@
+package org.tnsif.acc.c2tc.scannerclass_bufferreaderorg.tnsif.acc.c2tc.sr_developer;
+
+
+public class KIA_Internal {
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+		
+
+	}
+
+}
+
+}
