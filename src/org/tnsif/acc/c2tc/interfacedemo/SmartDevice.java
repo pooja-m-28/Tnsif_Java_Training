9c9fe8e6003eb841e42fde3d0847c11ee0ea0e63
Pooja M <Pooja M@POOJA-M>
2026-09-22 10:44:27 +0530

    First Commit

---
+++ src/org/tnsif/acc/c2tc/interfacedemo/SmartDevice.java
@@ -0,0 +1,7 @@
+package org.tnsif.acc.c2tc.interfacedemo;
+
+public interface SmartDevice {
+	void turnOn();
+	void turnOff();
+	void getStatus();
+}
---
+++ src/org/tnsif/acc/c2tc/multipleinterface/MusicPlayer.java
@@ -0,0 +1,6 @@
+package org.tnsif.acc.c2tc.multipleinterface;
+
+public class MusicPlayer {
+	void playMusic();
+
+}
